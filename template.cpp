a60ab456fe6857a581682a9730ad17862cadb692
杨祥钰 <2049161978@qq.com>
2018-10-10 17:32:38 +0800

    顺序表模板(int 和string）)

--- template.cpp
+++ template.cpp
@@ -37,9 +37,14 @@ public:
 			{
 				delete[] _array; //把旧的释放
 				_array = new T[s._size];
-				memcpy(_array, s._array, sizeof(T)*s._size);
+				//memcpy(_array, s._array, sizeof(T)*s._size);
+				for (size_t i = 0; i < s._size; i++)
+				{
+					_array[i] = s._array[i];  
+					//如果是string类，就是赋值，string的赋值是深拷贝，同理下面的insert扩容拷贝
+				}
 			}
-			else  //即s对象里没有数据，如果给this._array开空间
+			else  //即s对象里没有数据，如果给this._array开空间,那么this_array就有空间，而s.array没有空间，就不是赋值
 				_array = NULL;
 			_capacity = _size = s._size;		
 		}
@@ -47,14 +52,15 @@ public:
 	}
 
 	//Seqlist<T>& operator=( Seqlist<T> s) //可以用类型，也可以用类名
-	Seqlist& operator=(Seqlist s)  //赋值的现代写法
-	{
-		swap(_array, s._array);
-		swap(_size, s._size);
-		swap(_capacity, s._capacity);
-		return *this;
-	    //由于this的_array和s._array交换，那么出了该栈帧后，对象s会调析构函数，那么旧的_array会被释放
-	}
+	//Seqlist& operator=(Seqlist s)  //赋值的现代写法
+	//{
+	//	swap(_array, s._array);
+	//	swap(_size, s._size);
+	//	swap(_capacity, s._capacity);
+	//	return *this;
+	//    //由于this的_array和s._array交换，那么出了该栈帧后，对象s会调析构函数，那么旧的_array会被释放
+	//}
+
 	void Insert(size_t pos,const T& x) //在pos位置插入元素
 	{
 		assert(pos<=_size);//可以在_size位置插入元素，相当于尾插
@@ -69,7 +75,16 @@ public:
 				_capacity = _capacity * 2;
 			}
 			T* newarray = new T[_capacity];
-			memcpy(newarray, _array, sizeof(T)*_size);
+			/*memcpy(newarray, _array, sizeof(T)*_size);*/
+			//如果数组里是string类，用注释的拷贝方法，当扩容时，memcpy是值拷贝，
+			//那么新的newarray里的元素会指向相同的字符串空间，
+			//但是delete[] _array后，由于_array里存的是string类，类也会完成清理，
+			//那么就会将对象指向的那段字符串空间释放掉,但是当main函数结束时，
+			//会调析构函数，新的_array会再次清理对象，就会造成对同一段空间的两次释放
+			for (size_t i = 0; i < _size; i++)
+			{
+				newarray[i] = _array[i];//string类的赋值是深拷贝
+			}
 			delete[] _array;
 			_array = newarray;
 		}
@@ -191,10 +206,31 @@ void Test()
 	}
 
 }
+void TestString()
+{
+	Seqlist<string> s1;
+	s1.PushBack("abcd");
+	s1.PushBack("ab"); 
+	s1.PushBack("mngg");
+	s1.PushBack("loj");
+	//s1.PushBack("logh");
+	for (size_t i = 0; i < s1.Size(); i++)
+	{
+		cout << s1[i] << " ";
+	}
+	printf("\n");
+	Seqlist<string> s2;
+	s2 = s1;
+	for (size_t i = 0; i < s2.Size(); i++)
+	{
+		cout << s2[i] << " ";
+	}
+}
 int main()
 {
 	//TestInt();
-	Test();
+	//Test();
+	TestString();
 	system("pause");
 	return 0;
 }
@@ -203,27 +239,27 @@ int main()
 //Seqlist 是类名
 //Seqlist<T>是类型
 
-template<class T>
-class Seqlist 
-{
-public:
-	//Seqlist()  //顺序表模板的构造函数，在类里定义，用的类名
-	//	:_array(NULL)
-	//	,_capacity(0)
-	//	,_size(0)
-	//{
-	//}
-	Seqlist();//在类里声明
-private:
-	T* _array;
-	size_t _capacity;
-	size_t _size;
-};
-template<class T>  //必须重新定义一个类模板参数列表
-Seqlist<T>::Seqlist()//构造函数在类外定义，必须加上<T>，即用类型
-	:_array(NULL)
-	,_capacity(0)
-	,_size(0)
-{
-}
-
+//template<class T>
+//class Seqlist 
+//{
+//public:
+//	//Seqlist()  //顺序表模板的构造函数，在类里定义，用的类名
+//	//	:_array(NULL)
+//	//	,_capacity(0)
+//	//	,_size(0)
+//	//{
+//	//}
+//	Seqlist();//在类里声明
+//private:
+//	T* _array;
+//	size_t _capacity;
+//	size_t _size;
+//};
+//template<class T>  //必须重新定义一个类模板参数列表
+//Seqlist<T>::Seqlist()//构造函数在类外定义，必须加上<T>，即用类型
+//	:_array(NULL)
+//	,_capacity(0)
+//	,_size(0)
+//{
+//}
+//
