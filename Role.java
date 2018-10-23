9125da4d37c0ad774a45e4a673503e6ecbf7e901
杨祥钰 <2049161978@qq.com>
2018-10-23 20:47:46 +0800

    水仙花

---
+++ Role.java
@@ -0,0 +1,16 @@
+public class Role
+{
+     public static void main(String[] args)
+     {
+         for(int i=100;i<1000;i++)
+         {
+             int a=i/100;
+             int b=i%100/10; 
+             //int c=i%100%10;   // int c=i%10
+             if(i==((a*a*a)+(b*b*b)+(c*c*c)))
+             {
+                 System.out.print(i+"、");
+             }
+         }
+     }
+}
