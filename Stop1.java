78a64122226fb6d387915f109098f8f4f33d77fb
杨祥钰 <2049161978@qq.com>
2018-11-12 23:26:01 +0800

    线程停止

--- Stop1.java
+++ Stop1.java
@@ -4,38 +4,38 @@ package CODE.多线程;
 
 
 ////使用标记位
-//class Mythread3 implements  Runnable
-//{
-//    private boolean flag=true;
-//    public void run()
-//    {
-//        int i=1;
-//        while(flag)
-//        {
-//            try {
-//                Thread.sleep(1000);
-//                System.out.println(i+"次进入for循环" );
-//                i++;
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    public void setFlag(boolean flag) {
-//        this.flag = flag;
-//    }
-//}
-//public class Stop1
-//{
-//    public static void main(String[] args) throws InterruptedException {
-//        Mythread3 thread=new Mythread3();
-//        Thread thread1=new Thread(thread,"子线程");
-//        thread1.start();
-//        Thread.sleep(4000); //主线程4秒后会将thread1的falg置false,那么就会推出循环，子线程就会停止
-//        thread.setFlag(false);
-//    }
-//}
+class Mythread3 implements  Runnable
+{
+    private boolean flag=true;
+    public void run()
+    {
+        int i=1;
+        while(flag)
+        {
+            try {
+                Thread.sleep(1000);
+                System.out.println(i+"次进入for循环" );
+                i++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+}
+public class Stop1
+{
+    public static void main(String[] args) throws InterruptedException {
+        Mythread3 thread=new Mythread3();
+        Thread thread1=new Thread(thread,"子线程");
+        thread1.start();
+        Thread.sleep(4000); //主线程4秒后会将thread1的falg置false,那么就会推出循环，子线程就会停止
+        thread.setFlag(false);
+    }
+}
 
 
 //////使用stop
@@ -91,40 +91,41 @@ package CODE.多线程;
 //}
 
 
-////使用Thread.interupt方法分
+////使用Thread.interupt方法
 
-class Mythread3 implements  Runnable
-{
-
-    public void run() {
-        int i = 1;
-        while (true) {
-            try {
-                Thread.sleep(1000);
-                boolean bool = Thread.currentThread().isInterrupted(); //当前线程是否被置为中断状态，是返回ture
-                if (bool) {
-                    System.out.println("非阻塞情况下执行该操作....线程状态" + bool);
-                    break;
-                }
-                System.out.println("第" + i + "次执行，线程名称为:" + Thread.currentThread().getName());
-                i++;
-            } catch (InterruptedException e) {
-                boolean bool = Thread.currentThread().isInterrupted();
-                System.out.println(bool);
-                return;
-            }
-
-        }
-    }
-
-}
-public class Stop1
-{
-        public  static void main(String[] args) throws InterruptedException {
-            Mythread3 thread = new Mythread3();
-            Thread thread1 = new Thread(thread, "子线程");
-            thread1.start();
-            Thread.sleep(4000); //主线程4秒后会将thread1的falg置false,那么就会推出循环，子线程就会停止
-           thread1.interrupt();
-        }
-}
+//class Mythread3 implements  Runnable
+//{
+//    public void run() {
+//        int i = 1;
+//        while (true) {
+//            try {
+//                Thread.sleep(1000);
+//                boolean bool = Thread.currentThread().isInterrupted();//当前线程是否被置为中断状态，是返回ture
+//                //如果状态被置为中断状态，并且处于阻塞状态，会抛异常java.lang.InterruptedException，catch处理异常，不会走这个if语句
+//                if (bool) {
+//                    System.out.println("非阻塞情况下执行该操作....线程状态" + bool);
+//                    break;
+//                }
+//                System.out.println("第" + i + "次执行，线程名称为:" + Thread.currentThread().getName());
+//                i++;
+//            } catch (InterruptedException e) {
+//                //抛异常后，中断标志被系统会自动清除，线程中断状态由true变为false
+//                boolean bool = Thread.currentThread().isInterrupted();
+//                System.out.println(bool);  //false
+//                return;  //退出run方法
+//            }
+//
+//        }
+//    }
+//
+//}
+//public class Stop1
+//{
+//        public  static void main(String[] args) throws InterruptedException {
+//            Mythread3 thread = new Mythread3();
+//            Thread thread1 = new Thread(thread, "子线程");
+//            thread1.start();
+//            Thread.sleep(4000);
+//           thread1.interrupt();//现在线程thread1为中断状态
+//        }
+//}
