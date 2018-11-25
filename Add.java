80ce709929c9bcf9fe63dc7806007b0a2506f8f5
杨祥钰 <2049161978@qq.com>
2018-11-25 14:28:37 +0800

    leetcode 两数相加--转整型（有限制)

--- Add.java
+++ Add.java
@@ -1,186 +1,119 @@
-package CODE.文档管理系统;
+package CODE.链表;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
+class Mylink2 {
+    public ListNode head;
 
-//添加类
-public class Add extends JFrame implements ActionListener {
-    private JLabel lable1;  //标签1：文件名
-    private JLabel lable2;  //标签2：作者
-    private JLabel label3;  //标签3：文件大体内容
-
-    private JTextField nameText; //文本框：输入文件名
-    private JTextField authorText; //文本框：输入作者
-    private JTextField contentText; //文本框：内容
-
-    private JButton button;  //添加按钮
-
-    public Add() {
-        //设置添加窗口大小
-        this.setSize(400, 200);
-        this.setTitle("添加文档信息");
-        this.setLayout(null);
-
-        //放入文件名
-        lable1 = new JLabel("文件名");
-        lable1.setSize(50, 20);
-        lable1.setLocation(100, 30);
-        this.add(lable1);
-
-        //放入出入文件名文本框
-        nameText = new JTextField();
-        nameText.setSize(100, 20);
-        nameText.setLocation(145, 30);
-        this.add(nameText);
-
-        //放入作者标签
-        lable2 = new JLabel("作者");
-        lable2.setSize(50, 20);
-        lable2.setLocation(115, 52);
-        this.add(lable2);
-
-        //放入输入作者文本框
-        authorText = new JTextField();
-        authorText.setSize(100, 20);
-        authorText.setLocation(145, 52);
-        this.add(authorText);
-
-        //放入内容标签
-        label3 = new JLabel("内容");
-        label3.setSize(50, 20);
-        label3.setLocation(115, 73);
-        this.add(label3);
-
-        //放入输入内容文本框
-        contentText = new JTextField();
-        contentText.setSize(100, 20);
-        contentText.setLocation(145, 73);
-        this.add(contentText);
-
-        button = new JButton("添加");
-        button.setSize(60, 20);
-        button.setLocation(160, 110);
-        button.addActionListener(this);
-        this.add(button);
-
-
-        this.setVisible(true);
+    class ListNode {
+        private int val;
+        private ListNode next;
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (isEmpty()) {
-            JOptionPane.showMessageDialog(this, "文件名、作者或内容为空");
-            clear();
-            return ;
-        }
-        if (isError()) {
-            JOptionPane.showMessageDialog(this, "文件名、或内容超出字符长度限制");
-            clear();
-            return ;
-        }
-
-        //添加信息
-        StringBuffer info = new StringBuffer();
-        info.append(nameText.getText().trim() + "、");
-        info.append(authorText.getText().trim() + "、");
-        info.append(contentText.getText().trim() );
-
-        if (!addInfo(info.toString())) {
-            JOptionPane.showMessageDialog(this, "添加失败,该文件已存在");
-            clear();
-        }
-        else
-        {
-            JOptionPane.showMessageDialog(this, "添加成功");
-            clear();
-        }
-    }
-
-    //判断用户输入是否为空
-    private boolean isEmpty() {
-        if ("".equals(nameText.getText().trim()) || nameText.getText() == null) {
-            return true;
-        } else if ("".equals(authorText.getText().trim()) || authorText.getText() == null)
-            return true;
-        else if ("".equals(contentText.getText().trim()) || contentText.getText() == null)
-            return true;
-        return false;
-    }
-
-    private boolean isError() {
-        //文件名长度不能超过10
-        if (nameText.getText().length() > 10)
-            return true;
-        if (contentText.getText().length() > 20)
-            return true;
-        return false;
-    }
-
-    private void clear()
-    {
-        nameText.setText("");
-        authorText.setText("");
-        contentText.setText("");
-    }
-
-    //添加信息
-    private boolean addInfo(String info) {
-        String[] strs = info.split("、");
-        //文件名存在，直接返回false
-        if (nameExist(strs[0])) {
-            return false;
-        }
-        //开始添加文件
-        try {
-            FileWriter fw=new FileWriter("d:\\info.txt",true);
-            //创建一个文件的字符输出流，第二个参数表示是在文件尾进行追加
-            BufferedWriter bw=new BufferedWriter(fw);
-            //以上面创建的对象为参数创建一个缓冲字符输出流
-            bw.newLine();
-            bw.write(info);
-            bw.close();;
-            fw.close();
-            //创建新的一行
-        } catch (IOException e) {
-            System.out.println("文件写错误");
-        }
-        return true;
-    }
-
-    /*
-    判断文件名是否重复
-     */
-    private boolean nameExist(String nameText) {
-        boolean result = false;
-        try {
-            FileReader fr = new FileReader("d:\\info.txt");
-            //产生一个文件字符输入流对象
-            BufferedReader br = new BufferedReader(fr);
-            //以上一个对象作为参数来创建一个缓冲的字符输入流对象
-            String[] str = br.readLine().split("、");
-            while (str[0] != null) {
-                if (str[0].equals(nameText)) //判断是否以str开头
-                {
-                    result = true;
-                    break;
-                }
-                String n=br.readLine();
-                if(n!=null)
-                  str= n.split("、");
-                else
-                    break;
+    public void add(int val) {
+        if (head == null) {
+            head = new ListNode();
+            head.val = val;
+            head.next = null;
+        } else {
+            ListNode tail = head;
+            while (tail.next != null) {
+                tail = tail.next;
             }
-            br.close();
-            fr.close();
-            //读取一行数据
-        } catch (FileNotFoundException e) {
-            System.out.println("文件未找到");
-        } catch (IOException e) {
-            System.out.println("文件读错误");
+            ListNode newnode = new ListNode();
+            newnode.val = val;
+            newnode.next = null;
+            tail.next = newnode;
+            tail = newnode;
         }
-        return result;
     }
+
+    public void print() {
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.println(cur.val);
+            cur = cur.next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head1 = l1;
+        ListNode head2 = l2;
+        //l1存的是0
+        if (head1.val == 0 && head1.next == null)
+            return l2;
+        //l2存的是0
+        if (head2.val == 0 && head2.next == null)
+            return l1;
+        //接下来2个数字都不为0
+        long num1 = 0;
+        long flag = 1;
+        while (head1 != null) {
+            num1 += head1.val * flag;
+            flag *= 10;
+            head1 = head1.next;
+        }
+        long num2 = 0;
+        flag = 1L;
+        while (head2 != null) {
+            num2 += head2.val * flag;
+            flag *= 10;
+            head2 = head2.next;
+        }
+        long sum = num1 + num2;
+        //获取到sum的每一位（从低位到高位）
+        int[] bit = new int[32];
+        int i = 0;
+        while (sum > 0) {
+            bit[i] =(int) (sum % 10);
+            sum = sum / 10;
+            i++;
+        }
+        int counts = i; //一共有counts位
+        i = 0;
+        //将每一位进行尾插
+        ListNode newnode = null;
+        ListNode cur = newnode;
+        while (counts > 0) {
+            ListNode node = new ListNode();
+            node.val = bit[i];
+            node.next = null;
+            i++;
+
+            if (newnode == null) {
+                newnode = node;
+                cur = newnode;
+            } else {
+                cur.next = node;
+                cur = node;
+            }
+            counts--;
+        }
+        if (cur != null)
+            cur.next = null;
+        return newnode;
+    }
+
+}
+public class Add
+{
+    public static void main(String[] args) {
+        Mylink2 l1 = new Mylink2();
+        l1.add(9);
+
+
+        Mylink2 l2 = new Mylink2();
+        l2.add(1);
+        l2.add(9);
+        l2.add(9);
+        l2.add(9);
+        l2.add(9);
+        l2.add(9);
+        l2.add(9);
+        l2.add(9);
+        l2.add(9);
+        l2.add(9);
+        l1.addTwoNumbers(l1.head,l2.head);
+
+
+    }
+
 }
