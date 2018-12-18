ad6dd2ab5f929d2672f7c52fb6b5be3b2da67dcd
杨祥钰 <2049161978@qq.com>
2018-12-18 22:58:47 +0800

    Vector

---
+++ Vector1.java
@@ -0,0 +1,20 @@
+package CODE.类集;
+
+import java.util.List;
+import java.util.Vector;
+
+public class Vector1 {
+    public static void main(String[] args) {
+        List<String> list=new Vector<>();
+        list.add("hai");
+        list.add("pick");
+        list.add("health");
+        System.out.println(list);//[hai, pick, health]
+        list.remove("pick");
+        list.set(0,"hello");
+        for(int i=0;i<list.size();i++)
+        {
+            System.out.println(list.get(i)); //hello health
+        }
+    }
+}
